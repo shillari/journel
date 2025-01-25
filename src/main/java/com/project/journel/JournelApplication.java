215afa27ab496a79166f1bba1d0aeab5c4c65c66
shillari <sandy_hmh2@hotmail.com>
2025-01-25 20:31:34 +0100

    added oauth authentication and login and register endpoints

--- src/main/java/com/project/journel/JournelApplication.java
+++ src/main/java/com/project/journel/JournelApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JournelApplication {
 
 	public static void main(String[] args) {
-		System.out.println("POSTGRES_USERNAME: " + System.getenv("POSTGRES_USERNAME"));
-
 		SpringApplication.run(JournelApplication.class, args);
 	}
 
---
+++ src/main/java/com/project/journel/repository/UserAccountRepository.java
@@ -0,0 +1,12 @@
+package com.project.journel.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.journel.entity.database.UserAccount;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Integer> {
+
+  Optional<UserAccount> findByEmail(String email);
+}
